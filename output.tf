033eb7f84d2cfbf1dd7b516fc3784c5c804ff513
Mohammad Karimi <mkarimidevops@gmail.com>
2019-12-18 19:07:39 -0600

    output with new request

--- output.tf
+++ output.tf
@@ -1,3 +1,14 @@
 output "bucket_name"  {
 value = aws_s3_bucket.b.bucket
 }
+
+output "bucket_name"  {
+value = aws_security_group.allow_ssh.name
+}
+output "sec_group"  {
+value = aws_s3_bucket.b.bucket
+}
+
+output "key_name"  {
+value = aws_key_pair.deployer.key_name
+}
