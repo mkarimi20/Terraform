50693af7180ed53005faff5174d419c6b2087448
Mohammad Karimi <mkarimidevops@gmail.com>
2019-12-21 12:31:02 -0600

    spacing corrected 2

--- instance.tf
+++ instance.tf
@@ -1,31 +1,29 @@
-resource "aws_instance" "web" { 
+resource "aws_instance" "web" {
   count = var.count_instance
-  ami           = var.ami
-  instance_type = var.instance_type
+  ami             = var.ami
+  instance_type   = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
-resource "aws_instance" "web" { 
-  ami           = "AMI_ID_HERE" 
-  instance_type = "t2.micro" 
-  provisioner "remote-exec" { 
-    connection { 
-      host        = self.public_ip
-      type        = "ssh" 
-      user        = var.user
+  provisioner "remote-exec" {
+    connection {
+      host        = self.public_ip
+      type        = "ssh"
+      user        = var.user
       private_key = file(var.ssh_key_location)
       }
-      inline = [ 
-        "sudo yum install -y epel-release", 
+      inline = [
+        "sudo yum install -y epel-release",
         "sudo yum install httpd -y ",
-        "systemctl start httpd"
-        ] 
-      } 
-tags  { 
-    Name = "HelloWorld" 
-  } 
-   lifecycle{
+        "systemctl start httpd",
+        ]
+      }
+  lifecycle{
     prevent_destroy = false
   }
+  tags = {
+    Name = "HelloWorld${count.index +1}"
+  }
+}
 } 
 
