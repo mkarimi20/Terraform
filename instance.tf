6932ba4d59d588b08870f0055f36ca934338635d
Mohammad Karimi <mkarimidevops@gmail.com>
2019-12-21 12:00:53 -0600

    imported deleted

--- instance.tf
+++ instance.tf
@@ -13,9 +13,4 @@ tags = {
     prevent_destroy = false
   }
 } 
-  resource "aws_instance" "imported" {
-  ami           = "ami-00068cd7555f543d5" 
-  key_name = aws_key_pair.deployer.key_name
-  security_groups = ["allow_ssh"]
-  instance_type = "t2.micro" 
-}
+
