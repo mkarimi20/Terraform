f525681769fde9c94fba2e0ebb7a964fd3b4759a
Mohammad Karimi <mkarimidevops@gmail.com>
2019-12-21 10:39:19 -0600

    updated imported

--- instance.tf
+++ instance.tf
@@ -10,7 +10,7 @@ tags = {
     Name = "HelloWorld${count.index +1}" 
   } 
 } 
-  resource "aws_instace" "imported" {
+  resource "aws_instance" "imported" {
   ami           = "ami-00068cd7555f543d5" 
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
