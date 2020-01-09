1404dfd8da14bacb2f476fce4f0c1353627395c0
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-08 19:50:54 -0600

    spacing corrected

--- data_source/tower.tf
+++ data_source/tower.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-region = "us-east-1" 
-}
-
 data "aws_ami" "centos" {
     filter {
     name = "root-device-type" 
@@ -21,20 +17,20 @@ value = data.aws_ami.centos.id
 }
 
 resource "aws_key_pair" "towerkey" {
-    key_name   = "towerkey"
-  public_key = file("~/.ssh/id_rsa.pub")
+    key_name = "towerkey"
+    public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.centos.id
+resource "aws_instance" "tower" {
+  ami = data.aws_ami.centos.id
   instance_type = "t2.micro"
-  key_name  = aws_key_pair.towerkey.keyname
+  key_name = aws_key_pair.towerkey.keyname
 
 provisioner "remote-exec" {
     connection {
-        host        = self.public_ip
-        type        = "ssh"
-        user        = "centos"
+        host = self.public_ip
+        type = "ssh"
+        user = "centos"
         private_key = file("~/.ssh/id_rsa")
         }
     inline = [
