d5fbb608ee2ad38ea26265726fb13f84279bf664
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-08 18:51:40 -0600

    ec2 resource added

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -11,3 +11,12 @@ owners = ["099720109477"]
 output "ami" { 
 value = "${data.aws_ami.ubuntu.id}"
 }
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
