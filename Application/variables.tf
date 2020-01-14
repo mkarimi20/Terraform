e913ca95b7bb97e6796871967e7293c0cff95e96
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-13 19:31:17 -0600

    subnet changed

--- Application/variables.tf
+++ Application/variables.tf
@@ -5,7 +5,7 @@ variable "cider" {}
 variable "azs1" {}
 variable "azs2" {}
 variable "azs3" {}
-variable "priv-subnet2" {}
+variable "priv-subnet1" {}
 variable "priv-subnet2" {}
 variable "priv-subnet3" {}
 
