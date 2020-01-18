fcebe11459ae77e0c2ed9e5fb61623edb4bcb80f
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-18 12:56:03 -0600

    info added

--- azure/backend.tf
+++ azure/backend.tf
@@ -4,5 +4,7 @@ terraform {
     storage_account_name  = "terraform1karimi"
     container_name        = "tfstate"
     key                   = "prod_terraform.tfstate"
+    subscriptions_id      = "b37fc0e7-38da-496e-a832-1f208d980079"
+    tenant_id             = "46c32b48-cb6c-423d-b4c7-006b8fed7270"
   }
 }
