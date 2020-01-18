88bb13d8f94238729953d1e0aa4a111c849bd408
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-18 12:50:28 -0600

    backend created for azure

---
+++ azure/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "example"
+    storage_account_name  = "terraform1karimi"
+    container_name        = "tfstate"
+    key                   = "prod_terraform.tfstate"
+  }
+}
